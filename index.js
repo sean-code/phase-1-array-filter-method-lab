e5699f6f7e269999ecd15f985a5d5ded2de5f1f1
sean-code <john.nganga@student.moringaschool.com>
2022-08-21 14:25:12 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const drivers = ['Tony', 'Hemsworth', 'Gallows', 'Epps', 'Kevin', 'Latimore']
 //return drivers passed in array
 
 //find the matching using arrow function
-const findMatching = (p,q) => p.filter(p => p.toLowerCase() == q.toLowerCase())
+const findMatching = (p,q) => p.filter(p => p.toLowerCase() === q.toLowerCase())
 
 //matchName
 const matchName = (p,q) => p.filter(p => p.name == q)
-//fuzzyMatch
+//fuzzyMatc
 const fuzzyMatch = (p,q) => p.filter(p => p.charAt(0) === q.charAt(0))
